9f8bf0847ec1f03ac619630e1b0751efb8489ead
Phil Pennock <phil+git@pennock-tech.com>
2026-03-26 20:43:18 -0400

    style: comment additions for previous commit

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,6 +2,12 @@
 // Package version holds build-time version information set via ldflags.
 package version
 
+// This is explicitly named screaming-case for matching with 'sed'.
+// DO NOT RENAME without also updating Taskfile.yml and checking other non-Go files for references to it.
+// Our release flow makes two commits to edit this.
+//
+// This should be "latest release + 0.0.1 with -dev suffix"
+// OR if at the point where we're tagging, then that explicit tag.
 const HARDCODED_VERSION = "v0.0.3-dev"
 
 // Set by ldflags at build time.
