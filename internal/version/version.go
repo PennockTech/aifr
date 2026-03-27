67cd4617c4e46b6b4ca07c4a1adf53c95739e63f
Phil Pennock <phil+git@pennock-tech.com>
2026-03-26 20:30:14 -0400

    feat: reproducible builds, embedded version
    Use the full reproducible builds variant per goreleaser, for safety.
    
    Capture the builtBy into the binary; at least `strings` will see it.
    
    Rather than just `dev` as the default version, make it be "v<next>-dev",
    and for a release bump it twice, tagging the first.  This logic mostly ripped
    from my github.com/philpennock/character repo, only adjusting to make the file
    parameterized.  NB: adds release dependency upon 'convco'.
    
    Also copy some tasks for things like invoking pinact, but remembering the
    `--min-age` defense step.

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,9 +2,12 @@
 // Package version holds build-time version information set via ldflags.
 package version
 
+const HARDCODED_VERSION = "v0.0.3-dev"
+
 // Set by ldflags at build time.
 var (
-	Version   = "dev"
+	Version   = HARDCODED_VERSION
 	Commit    = "unknown"
 	BuildDate = "unknown"
+	BuiltBy   = ""
 )
